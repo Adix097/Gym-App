23f05fda17bd8c3bb7389eaa709df39b3138964f
adix097 <adityav181216@gmail.com>
2026-01-15 21:34:23 +0530

    exercise page without camera && non-component based

---
+++ app/exercise.jsx
@@ -0,0 +1,80 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+
+import Spacer from "../components/Spacer";
+
+const exercise = () => {
+  let count = 10;
+  let status = 0;
+  return (
+    <SafeAreaView style={styles.page}>
+      <Spacer />
+      <Text style={styles.exerciseTitle}>Exercise name: Push ups</Text>
+      <Spacer height={80} />
+      <View style={styles.cameraFrame}>
+        <Text>Camera Frame</Text>
+      </View>
+      <Spacer height={120} />
+      <Text style={styles.countTitle}>COUNT</Text>
+      <Text style={styles.count}>{count}</Text>
+      <Spacer />
+      <View style={styles.statusView}>
+        <Text style={styles.status}>Status: </Text>
+        {status === 0 ? (
+          <Text style={[styles.status, { color: "rgb(0, 255, 0)" }]}>
+            tracking
+          </Text>
+        ) : (
+          <Text style={[styles.status, { color: "rgb(255, 0, 0)" }]}>
+            Unmonitored
+          </Text>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default exercise;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#222",
+  },
+  exerciseTitle: {
+    fontWeight: "bold",
+    fontSize: 28,
+    color: "#fff",
+    textAlign: "center",
+  },
+  cameraFrame: {
+    height: "40%",
+    width: "80%",
+    border: "2px solid white",
+    backgroundColor: "#fff",
+  },
+  countTitle: {
+    fontWeight: "bold",
+    fontSize: 28,
+    color: "#dddddd",
+    textAlign: "center",
+  },
+  count: {
+    fontWeight: "bolder",
+    fontSize: 64,
+    color: "#ffe043",
+    textAlign: "center",
+    fontFamily: "console",
+  },
+  statusView: {
+    flexDirection: "row",
+  },
+  status: {
+    fontWeight: "bold",
+    fontSize: 28,
+    color: "#fff",
+    textAlign: "center",
+  },
+});
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
+import React from "react";
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.page}>
+      <StatusBar style="light" />
+      <Link href="./exercise" style={styles.link}>
+        Go to exercise page →
+      </Link>
+    </SafeAreaView>
+  );
+};
 
-export default index
+export default index;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#222",
+  },
+  link: {
+    fontSize: 24,
+    fontWeight: "bold",
+    fontFamily: "Inter",
+    color: "#fff",
+    textDecorationLine: "underline",
+  },
+});
