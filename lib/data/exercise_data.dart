893429937c428eeb6a184ad9d9210cfae58ead86
Aditya <122256697+Adix097@users.noreply.github.com>
2026-01-25 14:43:11 +0530

    remove lib/data directory

--- lib/data/exercise_data.dart
+++
@@ -1,7 +0,0 @@
-import '../models/exercise_config.dart';
-
-const List<Exercise> exercises = [
-  Exercise(id: 0, name: 'Push Ups', target: 10),
-  Exercise(id: 1, name: 'Plank', target: 30),
-  Exercise(id: 2, name: 'Squats', target: 20),
-];
