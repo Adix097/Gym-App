d746020d30d2d94355c265f4897605959fa2597c
adix097 <adityav181216@gmail.com>
2026-01-26 17:22:09 +0530

    set the initial route to be always the landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-
 import 'pages/landing.dart';
 import 'pages/login.dart';
 import 'pages/register.dart';
 import 'pages/dashboard.dart';
-import 'services/auth_storage.dart';
 
 late List<CameraDescription> cameras;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
-  final loggedIn = await AuthStorage.isLoggedIn();
-  runApp(GymApp(startRoute: loggedIn ? "/dashboard" : "/landing"));
+  runApp(const GymApp());
 }
 
 class GymApp extends StatelessWidget {
-  final String startRoute;
-
-  const GymApp({super.key, required this.startRoute});
+  const GymApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: startRoute,
+      initialRoute: '/landing',
       routes: {
         '/landing': (context) => const LandingPage(),
         '/login': (context) => const LoginPage(),
